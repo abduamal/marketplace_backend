4bd6bf2737bca7e1965fe4b187e0e6f58f58e144
abduamal <abduamal@ymail.com>
2021-05-31 17:36:41 -0500

    seed Product and Shop tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,48 @@
 
 require 'faker'
 
-Shops.destroy_all
-puts "Shops have been reset." if Shop.count == 0
+# Product.destroy_all
+# puts "Products have been reset." if Product.count == 0
 
-User.destroy_all
-puts "Users have been reset." if User.count == 0
+# Shop.destroy_all
+# puts "Shops have been reset." if Shop.count == 0
 
-4.times do
-  User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.unique.last_name,
-    bio: Faker::String.random(length: 50..150),
-    image: Faker::LoremPixel.image(size: "500x500", is_gray: true, category: 'people'),
-    email: Faker::Internet.unique.free_email,
-    password_digest: "password"
-  )
-end
+# User.destroy_all
+# puts "Users have been reset." if User.count == 0
 
-4.times do
-  Shop.create!(
-    user_id: Faker::Number.between(from: User.first.id, to: User.last.id),
-    name: Faker::Company.unique.name,
-  )
-end
+# 4.times do
+#   User.create!(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.unique.last_name,
+#     bio: Faker::String.random(length: 50..150),
+#     image: Faker::LoremPixel.image(size: "500x500", is_gray: true, category: 'people'),
+#     email: Faker::Internet.unique.free_email,
+#     password_digest: "password"
+#   )
+# end
+
+# 2.times do
+#   Shop.create!(
+#     user_id: Faker::Number.between(from: User.first.id, to: User.last.id),
+#     name: Faker::Company.unique.name,
+#     industry: Faker::Company.industry
+#   )
+# end
+
+# Product.create!(
+#   user_id: 5,
+#   shop_id: 1,
+#   name: "Spanish Translating Device",
+#   price: 5000,
+#   description: "This product translates all spanish words and sound effects",
+#   quantity: 200
+# )
+
+# Product.create!(
+#   user_id: 3,
+#   shop_id: 2,
+#   name: "Where's Walter? An Autobiography.",
+#   price: 5,
+#   description: "This novel is written by me, Cody Walter",
+#   quantity: 20000
+# )
