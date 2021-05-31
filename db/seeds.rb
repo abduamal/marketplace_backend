178a1fb6f5b25024c3fc1f7ec72e82e3e94a1d3a
abduamal <abduamal@ymail.com>
2021-05-31 16:05:18 -0500

    Seed user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+puts "Users have been reset." if User.count == 0
+
+4.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.unique.last_name,
+    bio: Faker::String.random(length: 50..150),
+    image: Faker::LoremPixel.image(size: "500x500", is_gray: true, category: 'people'),
+    email: Faker::Internet.unique.free_email,
+    password_digest: "password"
+  )
+end
