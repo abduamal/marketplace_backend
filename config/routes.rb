46a80b15519c6ba1fca73f77270666c53ce810a6
abduamal <abduamal@ymail.com>
2021-06-01 09:18:08 -0500

    edit resources and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-Rails.application.routes.draw do
-  resources :products
-  resources :shops
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  resources :shops do
+    resources :products
+  end
+  #For the User Model, a user can only create an account aka signup:
+    # This is the sign up form
+  get '/signup' => 'users#new'
+    # This creates a new user in the database using the data provided by the user
+  post '/users' => 'users#create'
 end
