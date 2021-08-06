ae8dce12cbe37cc06751f12fd77323f9bbd3362b
Amal Abdu <abduamal@ymail.com>
2021-08-06 14:43:59 -0500

    change local variables into class instance variables

--- app/controllers/apiv2/products_controller.rb
+++ app/controllers/apiv2/products_controller.rb
@@ -1,43 +1,51 @@
 class Apiv2::ProductsController < ApplicationController
 
+  before_action :set_shop
+  before_action :find_product, only: [:show, :update, :destroy]
+
   def index
-    products = Product.all
-    render json: ProductSerializer.new(products)
+    @products = @shop.products
+    render json: ProductSerializer.new(@products)
   end
 
   def show
-    product = Product.find(params[:id])
-    render json: ProductSerializer.new(product)
+    render json: ProductSerializer.new(@product)
   end
 
   def create
-    product = Product.new(product_params)
-    if product.save
-      render json: ProductSerializer.new(product), status: :accepted
+    @product = Product.new(product_params)
+    if @product.save
+      render json: ProductSerializer.new(@product), status: :accepted
     else
-      render json: {errors: product.errors.full_messages}, status: :unprocessible_entity
+      render json: {errors: @product.errors.full_messages}, status: :unprocessible_entity
     end
   end
 
   def update
-    product = Product.find(params[:id])
-    if product.update(product_params)
-      render json: ProductSerializer.new(product), status: :accepted
+    if @product.update(product_params)
+      render json: ProductSerializer.new(@product), status: :accepted
     else
-      render json: {errors: product.errors.full_messages}, status: :unprocessible_entity
+      render json: {errors: @product.errors.full_messages}, status: :unprocessible_entity
     end
   end
 
   def destroy
-    product = Product.find(params[:id])
-    if product.destroy
-      render json: ProductSerializer.new(products)
+    if @product.destroy
+      render json: ProductSerializer.new(@products)
     else
-      render json: {errors: product.errors.full_messages}, status: :unprocessible_entity
+      render json: {errors: @product.errors.full_messages}, status: :unprocessible_entity
     end
   end
 
   private
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name, :price, :quantity, :user_id, :shop_id)
--- app/controllers/apiv2/shops_controller.rb
+++ app/controllers/apiv2/shops_controller.rb
@@ -1,43 +1,45 @@
 class Apiv2::ShopsController < ApplicationController
+  before_action :find_shop, only: [:show, :update, :destroy]
 
   def index
-    shops = Shop.all
-    render json: ShopSerializer.new(shops)
+    @shops = Shop.all
+    render json: ShopSerializer.new(@shops)
   end
 
   def show
-    shop = Shop.find(params[:id])
-    render json: ShopSerializer.new(shop)
+    render json: ShopSerializer.new(@shop)
   end
 
   def create
-    shop = Shop.new(shop_params)
-    if shop.save
-      render json: ShopSerializer.new(shop), status: :accepted
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      render json: ShopSerializer.new(@shop), status: :accepted
     else
-      render json: {errors: shop.errors.full_messages}, status: :unprocessible_entity
+      render json: {errors: @shop.errors.full_messages}, status: :unprocessible_entity
     end
   end
 
   def update
-    shop = Shop.find(params[:id])
-    if shop.update(shop_params)
-      render json: ShopSerializer.new(shop), status: :accepted
+    if @shop.update(shop_params)
+      render json: ShopSerializer.new(@shop), status: :accepted
     else
-      render json: {errors: shop.errors.full_messages}, status: :unprocessible_entity
+      render json: {errors: @shop.errors.full_messages}, status: :unprocessible_entity
     end
   end
 
   def destroy
-    shop = Shop.find(params[:id])
-    if shop.destroy
-      render json: ShopSerializer.new(shops)
+    if @shop.destroy
+      render json: ShopSerializer.new(@shops)
     else
-      render json: {errors: shop.errors.full_messages}, status: :unprocessible_entity
+      render json: {errors: @shop.errors.full_messages}, status: :unprocessible_entity
     end
   end
 
   private
+
+  def find_shop
+    @shop = Shop.find(params[:id])
+  end
 
   def shop_params
     params.require(:shop).permit(:name, :industry, :user_id)
