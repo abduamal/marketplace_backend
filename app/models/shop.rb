38fc9f18bc1e9d40918814f0b8ef181f32d3e03a
Amal Abdu <abduamal@ymail.com>
2021-08-06 09:13:14 -0500

    add validations to shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
-  belongs_to :user, optional: true 
+  belongs_to :user, optional: true
   has_many :products
+
+  validates_inclusion_of :industry, :in => ['Food', 'Retail', 'Entertainment', 'Health', 'Technology', 'Servive', 'Other']
 end
